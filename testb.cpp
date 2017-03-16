8df76862ab73ecb7c7927e551e4fb53cf7e04493
E24056823 <a7954997@gmail.com>
2017-03-16 22:19:08 +0800

    first try

---
+++ testb.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main(void) {
+int n,k,m;
+cin>>n;
+cout<<n<<endl;
+for
+(k=1;n!=1;k++) {
+
+m=n%2;
+
+
+ if(n==1) {   cout << n << endl; 
+ break;}
+ if(m==1)      
+{   n=3*n+1;
+   cout << n << endl;
+} if(m==0)
+{   n=n/2;
+   cout << n << endl;
+}
+}
+}
